bec6928949fa4866e382d67c07942be17537156f
Fantasmartu <fantasmartu@gmail.com>
2024-02-14 00:46:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ noBtn.addEventListener('mouseover', function(){
     noBtn.style.setProperty('left',randomX + '%');
     noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%`);
 })
+
+noBtn.addEventListener('click', function(){
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY + '%');
+    noBtn.style.setProperty('left',randomX + '%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%`);
+})
